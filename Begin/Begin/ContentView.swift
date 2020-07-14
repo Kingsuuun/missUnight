c99c58edeba3d8e156043499152ba69cbe4d9e8d
Kingsuuun <503053536@qq.com>
2020-07-14 16:38:07 +0800

    rename project

---
+++ Begin/Begin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Begin
+//
+//  Created by Bone on 2020/7/14.
+//  Copyright © 2020 Bone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
